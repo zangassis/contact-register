555acc872f81c146c6a846c1ce218daca8de964f
Assis Zang <assiszang@gmail.com>
2023-06-21 16:42:20 -0300

    Update BadExampleContactService.cs

--- Services/BadExampleContactService.cs
+++ Services/BadExampleContactService.cs
@@ -1,1 +1,17 @@
+using ContactRegister.Repository;
+using ContactRegister.Models;
 
+namespace ContactRegister.Services;
+
+public class ContactService {
+
+     private readonly IContactRepository _repository;
+
+    public ContactService()
+    {
+        _repository = new ContactRepository(); // Manual dependency creation
+    }
+
+   public List<Contact> FindAllContacts() =>
+       _repository.FindContacts();
+}
